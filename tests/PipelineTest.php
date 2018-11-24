b7115ed9afb95c1a0d0a0e6fe6b036044277eee4
ncou <nicolas_courade@yahoo.fr>
2018-11-24 12:42:52 +0100

    improve tests.

--- tests/PipelineTest.php
+++ tests/PipelineTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Pipe;
 
+use Psr\Container\ContainerInterface;
 use Chiron\Http\Psr\Response;
 use Chiron\Http\Psr\ServerRequest;
 use Chiron\Http\Psr\Uri;
@@ -47,6 +48,68 @@ class PipelineTest extends TestCase
         $pipeline = new Pipeline();
 
         $pipeline->pipe($middlewares);
+
+        $response = $pipeline->handle($this->request);
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertEquals('foobar', (string) $response->getBody());
+    }
+
+    public function testPipeCallableMiddleware()
+    {
+        $middlewares = [
+            function ($request, $handler) {
+                $response = $handler->handle($request);
+                $response->getBody()->write('bar');
+
+                return $response;
+            },
+            function ($request, $handler) {
+                $response = $handler->handle($request);
+                $response->getBody()->write('foo');
+
+                return $response;
+            },
+            new FixedResponseMiddleware(new Response(200)),
+        ];
+
+        $pipeline = new Pipeline();
+
+        $pipeline->pipe($middlewares);
+
+        $response = $pipeline->handle($this->request);
+
+        $this->assertInstanceOf(ResponseInterface::class, $response);
+        $this->assertEquals('foobar', (string) $response->getBody());
+    }
+
+    public function testPipeLazyMiddleware()
+    {
+        $middleware = new CallableMiddleware(function ($request, $handler) {
+                $response = $handler->handle($request);
+                $response->getBody()->write('foobar');
+
+                return $response;
+            });
+
+        $containerMock = $this->createMock(ContainerInterface::class);
+
+        $containerMock
+            ->expects($this->once())
+            ->method('has')
+            ->with('middlewareName')
+            ->willReturn(true);
+
+        $containerMock
+            ->expects($this->once())
+            ->method('get')
+            ->with('middlewareName')
+            ->willReturn($middleware);
+
+        $pipeline = new Pipeline($containerMock);
+
+        $pipeline->pipe('middlewareName');
+        $pipeline->pipe(new FixedResponseMiddleware(new Response(200)));
 
         $response = $pipeline->handle($this->request);
 
