d8b3b859b637421e86bf3377b225bb70d03d442f
ncou <nicolas_courade@yahoo.fr>
2020-11-18 18:23:56 +0100

    update

--- src/EmptyPipelineHandler.php
+++ src/EmptyPipelineHandler.php
@@ -14,7 +14,7 @@ final class EmptyPipelineHandler implements RequestHandlerInterface
 {
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
-        // TODO : créer une Exception\EmptyPipelineException ????  https://github.com/zendframework/zend-stratigility/blob/master/src/Exception/EmptyPipelineException.php
+        // TODO : créer une PipelineException, ou une Exception\EmptyPipelineException ????  https://github.com/zendframework/zend-stratigility/blob/master/src/Exception/EmptyPipelineException.php
         throw new OutOfBoundsException('Reached end of middleware queue. Does your controller return a response ?');
     }
 }
